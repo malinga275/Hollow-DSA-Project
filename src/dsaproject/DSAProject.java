3bc939f9ea6a25c201fab031706fe912c72f017a
malinga <gilbertmalinga@gmail.com>
2014-02-19 20:23:34 +0600

    complete insertion of nodes

--- src/dsaproject/DSAProject.java
+++ src/dsaproject/DSAProject.java
@@ -6,14 +6,24 @@ package dsaproject;
 
 /**
  *
- * @author gilbert
+ * @author malinga
  */
 public class DSAProject {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+      public static void main(String[] args) {
+        
+        BinaryTree t = new BinaryTree();
+        
+        t.addNode("java", 123, "gilbert", "grockz");
+        t.addNode("html", 124, "Albert", "poop");
+        t.addNode("kml", 156, "Lambert", "accif");
+        t.addNode("php", 178, "holms", "loofh");
+        t.addNode("abc", 133, "google", "eemt");
+        t.addNode("ice", 133, "google", "eemt");
+        t.addNode("kac", 133, "google", "eemt");
+        t.inOrder();
     }
 }
